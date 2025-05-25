8c02f7775f5a04aa25ac431b47e489f3a72bc9e3
Rahul Kr. Sahu <rahulksahu9110@gmail.com>
2025-05-25 14:52:34 +0530

    Added two new routes for getting a specific store and specific store items

--- app.py
+++ app.py
@@ -26,14 +26,28 @@ def create_store():
     return new_store, 201
 
 @app.post("/store/<string:name>/item")
-def add_items(name):
+def create_item(name):
     for store in stores:
         if store["name"] == name:
             request_data = request.get_json()
             new_item = {"name": request_data["name"], "price": request_data["price"]}
             store["items"].append(new_item)
             return new_item, 201
-    return {"message": "store not found"}, 404
+    return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
 
 if __name__ == '__main__':
     app.run(debug=True)
