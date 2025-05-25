ddedffe50b543616f658e377906ac6ff22e85b07
Rahul Kr. Sahu <rahulksahu9110@gmail.com>
2025-05-25 13:47:30 +0530

    Added a route to add new store

--- app.py
+++ app.py
@@ -18,5 +18,12 @@ stores = [
 def get_stores():
     return {"stores": stores}
 
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
 if __name__ == '__main__':
     app.run(debug=True)
