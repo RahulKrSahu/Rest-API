25d7c2c74991dd3fd9e8b4b741496baeb8d26b9e
Rahul Kr. Sahu <rahulksahu9110@gmail.com>
2025-06-01 16:47:38 +0530

    removed unwanted code from app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import datetime
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -61,4 +60,4 @@ def delete_store(name):
     return {"message": f"Store '{name}' deleted"}, 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
